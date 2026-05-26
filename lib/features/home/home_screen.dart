e749130a0d5a30a203911419d5118a6479bfce88
Marcin <marcin.kowalskile@gmail.com>
2026-05-26 15:55:54 +0200

    Riverpod test

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../core/providers/test_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Home"),
+            Text("Counter: $counter"),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).state++;
+              },
+              child: const Text("Increase"),
+            ),
           ],
         ),
       ),
